e08021afd8254ca354a4e413272351dfd26db817
beatrizfrocha <47789897+beatrizfrocha@users.noreply.github.com>
2020-05-23 19:07:01 +0100

    pontos 2 e 7 feitos

--- exercicio2.pl
+++ exercicio2.pl
@@ -31,6 +31,18 @@ caminho1(G,A,[Y|P1],P) :-
    adjacente(X,Y,G), \+ memberchk(X,[Y|P1]), caminho1(G,A,[X,Y|P1],P).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Selecionar apenas algumas das operadoras de transporte para um determinado percurso
+
+differ([], _, []).
+differ([(A,X)|Tail], L2, Result):- member(X, L2), !, differ(Tail, L2, Result). 
+differ([(A,X)|Tail], L2, Result):- differ(Tail, L2, Result1), Result = [A|Result1].
+
+inclui_op(G,[],[]).
+inclui_op(grafo(A,B),L,R) :- differ(A,L,R1), remove_aresta(B,R1,R2), R = grafo(A,R2). 
+
+trajeto_com_op(G,A,B,O,P) :- inclui_op(G,O,R), caminho(R,A,B,P).   
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Excluir um ou mais operadores de transporte para o percurso
 
 equals([], _, []).
@@ -46,3 +58,8 @@ exclui_op(G,[],G).
 exclui_op(grafo(A,B),L,R) :- equals(A,L,R1), remove_aresta(B,R1,R2), R = grafo(A,R2). 
 
 trajeto_sem_op(G,A,B,O,P) :- exclui_op(G,O,R), caminho(R,A,B,P).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Escolher o percurso que passe apenas por abrigos com publicidade
+
+trajeto_com_abrigo(G,A,B,P) :- exclui_op(G,['No'],R), caminho(R,A,B,P).
